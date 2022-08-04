356ee523cd76e9f10cbeb2a8b11a06285a1f576f
Cole <cole.baty@gmail.com>
2022-08-04 19:55:52 -0400

    python is weird

--- timesheet.py
+++ timesheet.py
@@ -1,5 +1,23 @@
+from datetime import *
+import time
+
 f = open("timesheet", 'r')
 
-print(f.readlines(-1))
+line = f.readlines()[-1]
+print(line)
+action = line.split()[-1]
+print(action)
+timestamp = line.split()[1:-1]
+print(timestamp)
+
+
+timestamp_date_str = ""
+for word in timestamp:
+    timestamp_date_str += word
+
+print(timestamp_date_str)
+    
+# timestamp_date = datetime.strftime(timestamp_date_str)
+# print(timestamp_date)
 
 f.close()
